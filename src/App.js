afc0314283bee7301055e8822841d2759b0a61e2
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2023-05-01 01:08:57 -0400

    initial html and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Weather from "./Weather";
+
+
 
 export default function App() {
-  return <div>Weather App
-    <footer>This project was coded by Courtney Perry and is 
+  return <div className="App">
+    <div className="container">
+       <h1>Weather App</h1>
+       <Weather />
+    <footer>This project was coded by Courtney Perry and is {""}
       <a href=" ">open-sourced on Github</a>
     </footer>
+    </div>
   </div>
 }
