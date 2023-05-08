43aafae68bcad552da6bd95057bb4c5221cbd8a2
cnjperrys <116315021+cnjperrys@users.noreply.github.com>
2023-05-08 11:30:47 -0400

    updated links

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
        <h1>Weather App</h1>
        <Weather defaultCity={"Boston"} />
     <footer>This project was coded by Courtney Perry and is {""}
-      <a href="https://github.com/cnjperrys/react-weather">open-sourced on Github</a>
+      <a href="https://github.com/cnjperrys/react-weather" target="_blank" rel="noopener noreferrer">open-sourced on Github</a>
     </footer>
     </div>
   </div>
